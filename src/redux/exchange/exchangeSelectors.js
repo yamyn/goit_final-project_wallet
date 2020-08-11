4458eabbcade5797d7fe51614baae54cf9ab9e8c
Ya_Myn <ricky.month@gmail.com>
2020-08-12 01:12:14 +0300

    add exchange reducer

---
+++ src/redux/exchange/exchangeSelectors.js
@@ -0,0 +1,2 @@
+export const getTime = state => state.exchange.time;
+export const getCurrencies = state => state.exchange.currencies;
---
+++ src/redux/walet/waletReducer.js
@@ -0,0 +1,25 @@
+import { createReducer, combineReducers } from '@reduxjs/toolkit';
+import { loadTransactionsSuccess, addTransactionSuccess } from './waletActions';
+
+import { logout } from '../session/sessionAction';
+
+const balance = createReducer(0, {
+    [loadTransactionsSuccess]: (state, action) => action.payload.balance,
+    [addTransactionSuccess]: (state, action) => action.payload.balance,
+});
+
+const transactions = createReducer([], {
+    [loadTransactionsSuccess]: (state, action) => action.payload.transactions,
+    [addTransactionSuccess]: (state, action) => [
+        ...state,
+        action.payload.transaction,
+    ],
+    [logout]: () => [],
+});
+
+const waletReducer = combineReducers({
+    balance,
+    transactions,
+});
+
+export default waletReducer;
